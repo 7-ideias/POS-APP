3663e9ef130f4baa7083939b557aef29b1b43134
Carlos Pijanowski Cartaxo <carlospc1978@gmail.com>
2023-09-05 09:58:33 -0300

    so formatacao

--- lib/utilitarios/utils.dart
+++ lib/utilitarios/utils.dart
@@ -3,6 +3,7 @@ import 'package:intl/intl.dart';
 import 'package:pos_app/utilitarios/VariaveisGlobais.dart';
 
 class Utils {
+
   static formataParaMoeda(double price) => '${VariaveisGlobais.moeda}${price.toStringAsFixed(2)}';
   static formatDate(DateTime date) => DateFormat.yMd().format(date);
 
